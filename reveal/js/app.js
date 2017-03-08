f930dc360485ff0c13b1ff3ecc808b1dbff80c51
Sam Pattnaik <sambit@uw.edu>
2017-03-08 01:28:12 -0800

    Formatting & info on the Side Drawer
    Added formatting & comments to all the code. Added info about the site
    on the side drawer.

--- reveal/js/app.js
+++ reveal/js/app.js
@@ -1,6 +1,7 @@
 /**
  * app.js
  * main application script
+ * Author - Sam Pattnaik
  */
 "use strict";
 
@@ -14,7 +15,8 @@ var submitButton = document.querySelector(".submit-button");
 
 /* This block gets the value of the radio button once submit button is clicked and returns the results based on the selected options*/
 submitButton.addEventListener("click", function(){
-console.log("button clicked"); /* line for debugging purposes */
+
+    /* These variables get the value of the radio buttons, by first checking the name and then checking if they are checked */
     var answer1 = document.querySelector("input[name=options1]:checked");
     var answer2 = document.querySelector("input[name=options2]:checked");
     var answer3 = document.querySelector("input[name=options3]:checked");
@@ -22,15 +24,16 @@ console.log("button clicked"); /* line for debugging purposes */
     var answer5 = document.querySelector("input[name=options5]:checked");
     var answer6 = document.querySelector("input[name=options6]:checked");
     
-    if (answer1 == null || answer2 == null || answer3 == null || answer4 == null || answer5 == null || answer6 == null) {
-        alert("Not so soon Padawan, answer all the quiz questions first!");
+    if (answer1 == null || answer2 == null || answer3 == null || answer4 == null || answer5 == null || answer6 == null) {  /*checks if all the radio buttons are checked - all questions answered? */
+        alert("Not so soon Padawan, answer all the quiz questions first!"); /* if not, throws error */
     }
-    else {
+    /* if all is good, runs the conditions and displays according to data from the array */
+    else {  
         document.querySelector(".results-card").style.display = "block";
         document.querySelector(".see-results-title").style.display = "block";
         document.querySelector(".scroll-down").style.display = "block";
         if(nameInput.value==""){
-                    resultOutput.textContent = "Hello padawan, " + RESULTS[10].title;
+                    resultOutput.textContent = "Hello padawan, " + RESULTS[10].title; /* This part checks, if no name is entered, addresses the user as Padawan */
                     resultOutputdesc.textContent = RESULTS[10].description;
                     document.querySelector(".result-image").style.background = "url('../reveal/img/smart.gif')";
                     document.querySelector(".result-image").style.backgroundRepeat = "no-repeat";
@@ -38,7 +41,7 @@ console.log("button clicked"); /* line for debugging purposes */
                     document.querySelector(".result-image").style.backgroundPosition = "center";
                 }
                 else {
-                    resultOutput.textContent = "Hello, " + nameInput.value + " " + RESULTS[10].title;
+                    resultOutput.textContent = "Hello, " + nameInput.value + ". " + RESULTS[10].title;
                     resultOutputdesc.textContent = RESULTS[10].description;
                     document.querySelector(".result-image").style.background = "url('../reveal/img/smart.gif')";
                     document.querySelector(".result-image").style.backgroundRepeat = "no-repeat";
@@ -56,7 +59,7 @@ console.log("button clicked"); /* line for debugging purposes */
 
                 }
                 else {
-                    resultOutput.textContent = "Hello, " + nameInput.value + " " + RESULTS[0].title;
+                    resultOutput.textContent = "Hello, " + nameInput.value + ". " + RESULTS[0].title;
                     resultOutputdesc.textContent = RESULTS[0].description;
                     document.querySelector(".result-image").style.background = "url('../reveal/img/saviour.gif')";
                     document.querySelector(".result-image").style.backgroundRepeat = "no-repeat";
@@ -74,7 +77,7 @@ console.log("button clicked"); /* line for debugging purposes */
                     document.querySelector(".result-image").style.backgroundPosition = "center";
                 }
                 else {
-                    resultOutput.textContent = "Hello, " + nameInput.value + " " + RESULTS[5].title;
+                    resultOutput.textContent = "Hello, " + nameInput.value + ". " + RESULTS[5].title;
                     resultOutputdesc.textContent = RESULTS[5].description;
                     document.querySelector(".result-image").style.background = "url('../reveal/img/lover.gif')";
                     document.querySelector(".result-image").style.backgroundRepeat = "no-repeat";
@@ -91,7 +94,7 @@ console.log("button clicked"); /* line for debugging purposes */
                     document.querySelector(".result-image").style.backgroundPosition = "center";
                 }
                 else {
-                    resultOutput.textContent = "Hello, " + nameInput.value + " " + RESULTS[9].title;
+                    resultOutput.textContent = "Hello, " + nameInput.value + ". " + RESULTS[9].title;
                     resultOutputdesc.textContent = RESULTS[9].description;
                     document.querySelector(".result-image").style.background = "url('../reveal/img/hope.gif')";
                     document.querySelector(".result-image").style.backgroundRepeat = "no-repeat";
@@ -108,7 +111,7 @@ console.log("button clicked"); /* line for debugging purposes */
                     document.querySelector(".result-image").style.backgroundPosition = "center";
                 }
                 else {
-                    resultOutput.textContent = "Hello, " + nameInput.value + " " + RESULTS[3].title;
+                    resultOutput.textContent = "Hello, " + nameInput.value + ". " + RESULTS[3].title;
                     resultOutputdesc.textContent = RESULTS[3].description;
                     document.querySelector(".result-image").style.background = "url('../reveal/img/hater.gif')";
                     document.querySelector(".result-image").style.backgroundRepeat = "no-repeat";
@@ -125,7 +128,7 @@ console.log("button clicked"); /* line for debugging purposes */
                     document.querySelector(".result-image").style.backgroundPosition = "center";
                 }
                 else {
-                    resultOutput.textContent = "Hello, " + nameInput.value + " " + RESULTS[4].title;
+                    resultOutput.textContent = "Hello, " + nameInput.value + ". " + RESULTS[4].title;
                     resultOutputdesc.textContent = RESULTS[4].description;
                     document.querySelector(".result-image").style.background = "url('../reveal/img/nature-lover.gif')";
                     document.querySelector(".result-image").style.backgroundRepeat = "no-repeat";
@@ -142,7 +145,7 @@ console.log("button clicked"); /* line for debugging purposes */
                     document.querySelector(".result-image").style.backgroundPosition = "center";
                 }
                 else {
-                    resultOutput.textContent = "Hello, " + nameInput.value + " " + RESULTS[7].title;
+                    resultOutput.textContent = "Hello, " + nameInput.value + ". " + RESULTS[7].title;
                     resultOutputdesc.textContent = RESULTS[7].description;
                     document.querySelector(".result-image").style.background = "url('../reveal/img/drunk.gif')"; 
                     document.querySelector(".result-image").style.backgroundRepeat = "no-repeat";
@@ -160,7 +163,7 @@ console.log("button clicked"); /* line for debugging purposes */
                     document.querySelector(".result-image").style.backgroundPosition = "center";
                 }
                 else {
-                    resultOutput.textContent = "Hello, " + nameInput.value + " " + RESULTS[8].title;
+                    resultOutput.textContent = "Hello, " + nameInput.value + ". " + RESULTS[8].title;
                     resultOutputdesc.textContent = RESULTS[8].description;
                     document.querySelector(".result-image").style.background = "url('../reveal/img/dreamer.gif')";
                     document.querySelector(".result-image").style.backgroundRepeat = "no-repeat";
@@ -177,7 +180,7 @@ console.log("button clicked"); /* line for debugging purposes */
                     document.querySelector(".result-image").style.backgroundPosition = "center";
                 }
                 else {
-                    resultOutput.textContent = "Hello, " + nameInput.value + " " + RESULTS[2].title;
+                    resultOutput.textContent = "Hello, " + nameInput.value + ". " + RESULTS[2].title;
                     resultOutputdesc.textContent = RESULTS[2].description;
                     document.querySelector(".result-image").style.background = "url('../reveal/img/workaholic.gif')";
                     document.querySelector(".result-image").style.backgroundRepeat = "no-repeat";
@@ -194,7 +197,7 @@ console.log("button clicked"); /* line for debugging purposes */
                     document.querySelector(".result-image").style.backgroundPosition = "center";
                 }
                 else {
-                    resultOutput.textContent = "Hello, " + nameInput.value + " " + RESULTS[1].title;
+                    resultOutput.textContent = "Hello, " + nameInput.value + ". " + RESULTS[1].title;
                     resultOutputdesc.textContent = RESULTS[1].description;
                     document.querySelector(".result-image").style.background = "url('../reveal/img/troublemaker.gif')";
                     document.querySelector(".result-image").style.backgroundRepeat = "no-repeat";
